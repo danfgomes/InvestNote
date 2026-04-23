ad32057066df079543b3e8e424fa77cf09bf2ac2
Daniel <dg1638382@gmail.com>
2026-04-23 18:11:31 -0300

    feat: configuracao final do workflow e ajustes no scraper

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from tavily import TavilyClient
 
 load_dotenv()
 
-# Agora ele puxa as chaves de forma invisível e segura
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
@@ -22,9 +21,9 @@ def enviar_mensagem_telegram(texto):
     response = requests.post(url, json=pack)
     
     if response.status_code == 200:
-        print("📱 Notificação enviada para o Telegram com sucesso!")
+        print(" Notificação enviada para o Telegram com sucesso!")
     else:
-        print(f"❌ Erro ao enviar para o Telegram: {response.text}")
+        print(f"Erro ao enviar para o Telegram: {response.text}")
 
 def buscar_noticias_com_ia():
     tavily = TavilyClient(api_key=TAVILY_API_KEY)
@@ -32,7 +31,7 @@ def buscar_noticias_com_ia():
     minha_carteira = ["PETR4", "NUBANK", "NVIDIA", "bitcoin", "ethereum"]
     acoes_texto = ", ".join(minha_carteira)
     
-    print(f"🤖 Acionando a IA do Tavily para ler sobre: {acoes_texto}...")
+    print(f"Acionando a IA do Tavily para ler sobre: {acoes_texto}...")
 
     pergunta_ia = f"Resuma as principais notícias de hoje focadas EXCLUSIVAMENTE nos seguintes ativos: {acoes_texto}. Ignore o resto do mercado. Escreva o resumo OBRIGATORIAMENTE em Português do Brasil (PT-BR)."
 
@@ -46,9 +45,9 @@ def buscar_noticias_com_ia():
     )
 
     resumo = resposta['answer']
-    print("\n📰 Resumo da carteira gerado. Disparando para o Telegram...")
+    print("\n Resumo da carteira gerado. Disparando para o Telegram...")
     
-    mensagem_final = f"📊 *InvesNote - Radar da Sua Carteira*\n🎯 Ativos: {acoes_texto}\n\n{resumo}"
+    mensagem_final = f" *InvesNote - Radar da Sua Carteira*\n Ativos: {acoes_texto}\n\n{resumo}"
     
     enviar_mensagem_telegram(mensagem_final)
 
